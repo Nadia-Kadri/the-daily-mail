65bfdbfab11d214c1e21cb79d1465b24568bd0b9
Nadia-Kadri <nadiagrace96@gmail.com>
2020-02-07 16:12:58 -0500

    scrape and clear

--- public/app.js
+++ public/app.js
@@ -1,9 +1,20 @@
-$("#viewscrape").on("click", function() {
-	$.getJSON("/api/articles", function(data) {
-		// For each one
-		for (let i = 0; i < data.length; i++) {
-			// Display the apropos information on the page
-			$("#scrapedArticles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
-		}
+$("#scrape").on("click", function() {
+
+	$.get("/api/scrape").then(function(data) {    
+    appendData(data)
 	});
+
+	$.get("/api/articles").then(function(data) {    
+    appendData(data)
+  });
+});
+
+function appendData (data) {
+	for (let i = 0; i < data.length; i++) {
+		$("#scrapedArticles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
+	}
+}
+
+$("#clear").on("click", function() {
+  $("#scrapedArticles").empty()
 });
