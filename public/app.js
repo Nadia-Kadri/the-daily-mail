629965af7faac162e96522c63b58932350666421
Nadia-Kadri <nadiagrace96@gmail.com>
2020-02-07 16:42:18 -0500

    scrape and home handlebars

--- public/app.js
+++ public/app.js
@@ -1,20 +1,3 @@
-$("#scrape").on("click", function() {
-
-	$.get("/api/scrape").then(function(data) {    
-    appendData(data)
-	});
-
-	$.get("/api/articles").then(function(data) {    
-    appendData(data)
-  });
-});
-
-function appendData (data) {
-	for (let i = 0; i < data.length; i++) {
-		$("#scrapedArticles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
-	}
-}
-
-$("#clear").on("click", function() {
-  $("#scrapedArticles").empty()
-});
+// $("#clear").on("click", function() {
+//   $("#scrapedArticles").empty()
+// });
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,6 +2,10 @@ module.exports = function(app) {
 
 	app.get("/", function(req, res) {
 		res.render("home");
-	});
-	
+  });
+  
+  app.get("/scrape", function(req, res) {
+		res.render("scrape");
+  });
+  
 };
