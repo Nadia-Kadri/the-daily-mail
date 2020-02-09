75fdcce40844c5dfd2eb30b9d43356e4150ff44f
Nadia-Kadri <nadiagrace96@gmail.com>
2020-02-08 21:17:29 -0500

    deploy heroku

--- server.js
+++ server.js
@@ -17,7 +17,8 @@ app.engine("handlebars", exphbs({defaultLayout: "main"}));
 app.set("view engine", "handlebars");
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/the-daily-mail", { useNewUrlParser: true });
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/the-daily-mail";
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 // Routes
 require("./routes/apiRoutes")(app);
