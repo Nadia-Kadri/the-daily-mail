56e0db95d374f4465262329a355080e2181bf9d8
Nadia-Kadri <nadiagrace96@gmail.com>
2020-02-08 21:07:44 -0500

    Add Note

--- public/js/scrape.js
+++ public/js/scrape.js
@@ -1,27 +1,61 @@
 $(document).ready(function() {
 
   $.get("/api/scrape").then(function(data) {    
-    appendData(data)
+    createCard(data)
 	});
 
 	$.get("/api/articles").then(function(data) {    
-    appendData(data)
+    createCard(data)
   });
 
 });
 
-function appendData (data) {
+function createCard (data) {
 	for (let i = 0; i < data.length; i++) {
 		$("#scrapedArticles").append(`
-      <div data-_id="${data[i]._id}" class="card">
+      <div class="card">
         <div class="card-header">
           <h3>
             <a class="article-link" target="_blank" href="${data[i].link}">${data[i].title}</a>
-            <a class="btn btn-success">Add a note</a>
           </h3>
         </div>
-        <div class="card-body">${data[i].summary}</div>
+        <div class="card-body">
+          ${data[i].summary}
+          <hr>
+          <div class="note-display" id="note-display${data[i]._id}">Article Notes:</div>
+          <div>Add/Update Note:</div>
+          <textarea class='bodyinput' id="note${data[i]._id}" name='body'></textarea><br>
+          <button type="button" class="btn btn-success add-note" id="${data[i]._id}">Submit</button>
+        </div>
       </div>
     `);
+
+    if (data[i].note) {
+      $(`#note-display${data[i]._id}`).html(`Article Note: ${data[i].note.body}`)
+    } else {
+      $(`#note-display${data[i]._id}`).html(`Article Note: You currently do not have a note saved for this article, add one below.`)
+    }
 	}
 }
+
+$(document).on("click", ".add-note", function() {
+  const articleId = $(this).attr("id")
+  const articleNote = $(`#note${articleId}`).val()
+
+  $.ajax({
+    method: "POST",
+    url: "/articles/" + articleId,
+    data: {
+      body: articleNote
+    }
+  })
+  .then(function(data) {
+    console.log(data);
+  });
+
+  $(`#note${articleId}`).val("");
+
+  $.get("/api/articles").then(function(data) {    
+    createCard(data)
+  });
+});
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -17,7 +17,7 @@ module.exports = function(app) {
         let summary = $(element).find("p").text();
         let link = $(element).find("a").attr("href");
 
-				if (title && link) {
+				if (title && summary && link) {
 					// Insert the data in the-daily-mail db
 					db.Article.create({title: title, summary: summary, link: link})
 					.then(data => console.log(data))
@@ -30,9 +30,20 @@ module.exports = function(app) {
 	app.get("/api/articles", function(req, res) {
     db.Article.find({})
       .limit(10)
+      .populate("note")
       // .sort("-_id")
 			.then(data => res.json(data))
 			.catch(err => res.json(err));
-	});
+  });
+
+  // Save/update an Article's associated Note
+  app.post("/articles/:id", function(req, res) {
+    db.Note.create(req.body)
+      .then(function(noteData) {
+        return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: noteData._id }, { new: true });
+      })
+      .then(articleData => res.json(articleData))
+      .catch(err => res.json(err));
+  });
 
 }
